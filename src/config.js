00b24e5ed34e0268fb7a079b0753ff8e0e49c172
danielschwartz4 <52050264+danielschwartz4@users.noreply.github.com>
2020-08-18 22:07:15 -0500

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+	apiGateway: {
+		REGION: 'YOUR_API_GATEWAY_REGION',
+		URL: 'YOUR_API_GATEWAY_URL'
+	},
+	cognito: {
+		REGION: 'us-east-2',
+		USER_POOL_ID: 'us-east-2_07GFS55wZ',
+		APP_CLIENT_ID: '7mbh7kspdvir96uq36ucm00n6j',
+		IDENTITY_POOL_ID: 'us-east-2:58be3594-8cdf-464e-8fb1-e9baaf8f6749'
+	}
+};
